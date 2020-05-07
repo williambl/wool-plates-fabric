538716913d84242bdac6d238bafcea63637b2a92
williambl <william@williambl.com>
2020-05-07 15:37:06 +0100

    Set up wool plates

---
+++ src/main/java/com/williambl/woolplates/WoolPlates.java
@@ -0,0 +1,10 @@
+package com.williambl.woolplates;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WoolPlates implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
